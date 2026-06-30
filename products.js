3fa8c44ddfb5524caf637de666a54fc80c4b817b
nolabelart <osofi654@gmail.com>
2026-06-30 17:52:07 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    id: 1,
+    name: "Abstract Horizon",
+    price: 29,
+    category: "abstract",
+    img: "/images/2.png",
+    description: "Abstract art for modern spaces"
+  },
+  {
+    id: 2,
+    name: "Minimal Nature",
+    price: 25,
+    category: "minimal",
+    img: "/images/1.png",
+    description: "Calm minimal nature design"
+  },
+  {
+    id: 3,
+    name: "Black & Light",
+    price: 32,
+    category: "black",
+    img: "/images/3.png",
+    description: "Black and white contrast design"
+    },
+  
+{
+    id: 4,
+    name: "Soft Lines",
+    price: 27,
+    category: "minimal",
+    img: "/images/4.png",
+    description: "Gentle line composition with soft curves and neutral colors."
+  },
+  {
+    id: 5,
+    name: "Dark Geometry",
+    price: 35,
+    category: "black",
+    img: "/images/5.png",
+    description: "Geometric dark composition with sharp forms and depth."
+  },
+  {
+    id: 6,
+    name: "Ocean Mood",
+    price: 30,
+    category: "abstract",
+    img: "/images/6.png",
+    description: "Abstract waves inspired by ocean colors and movement."
+  }
+]
