def9862a9f6dade981869f66252a41dc4a023296
nolabelart <osofi654@gmail.com>
2026-06-30 17:11:23 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const stripe = new Stripe("process.env.STRIPE_SECRET");
+
+app.post("/create-checkout", async (req, res) => {
+  const { cart } = req.body;
+
+  const line_items = cart.map(item => ({
+    price_data: {
+      currency: "eur",
+      product_data: {
+        name: item.name + " (" + item.size + ")"
+      },
+      unit_amount: item.price * 100
+    },
+    quantity: item.qty
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items,
+    mode: "payment",
+    
+success_url: "http://localhost:5173/success",
+cancel_url: "http://localhost:5173"
+
+  });
+
+  res.json({ url: session.url });
+});
+
+app.listen(3001, () => console.log("Server running on 3001"));
